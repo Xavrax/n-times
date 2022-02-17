930fc1ba63fa8f54ed063c0cd127f1d203c58019
Xavrax <1i13i.dahlke@gmail.com>
2022-02-17 20:32:58 +0000

    working repeater

---
+++ src/repeater.rs
@@ -0,0 +1,35 @@
+pub struct Repeater {
+    n: usize,
+}
+
+impl Repeater {
+    pub(crate) fn new(n: usize) -> Self {
+        Self {
+            n,
+        }
+    }
+
+    pub fn run<F>(&self, mut f: F) where F: FnMut() -> () {
+        for _ in 0..self.n {
+            f();
+        }
+    }
+}
+
+#[cfg(test)]
+mod should {
+    use super::*;
+    use test_case::test_case;
+
+    #[test_case(1)]
+    #[test_case(2)]
+    #[test_case(3)]
+    fn loop_for_n_times(n: usize) {
+        let mut counter = 0;
+
+        let sut = Repeater::new(n);
+        sut.run(|| counter += 1);
+
+        assert_eq!(counter, n)
+    }
+}
